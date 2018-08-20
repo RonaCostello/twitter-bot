34c374ba565a56bc9054c7fb0dc42903584e18c9
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-08-20 21:48:08 +0100

    Reduce the classification completion cutoff
    Decided to only include projects to tweet which have a lower completion
    score. Previously looking at projects with a completion of less than
    100%, this commit reduces that to 90%.

--- get-project-urls.py
+++ get-project-urls.py
@@ -6,11 +6,13 @@ import random
 def select_project():
     project_list = []
     for project in Project.where(launch_approved=True):
-        if project.completeness < 1:  # Project completeness ranges between 0 and 1
+        if project.completeness < 0.9:  # Project completeness ranges between 0 and 1
             project_list.append(project.title + ("__www.zooniverse.org/projects/" + project.slug))
+            # project_info.append([project.description, project.slug]) #For twitter message? Largest is 265
     random_project = select_random(project_list)
     write_out(random_project)
     return random_project
+
 
 def select_random(items):
     return random.choice(items)
@@ -19,10 +21,5 @@ def write_out(string):
     with open("last-promoted-project.txt", "a") as file:
         file.write(string + "\n")
 
-
-
-
-
-        # project_info.append([project.description, project.slug]) #For twitter message? Largest is 265
 
 print select_project()
