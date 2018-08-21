f8906246a38c8f5d8559f87950cda73fef884832
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-08-21 10:53:39 +0100

    Import the select random project module

--- tweetbot.py
+++ tweetbot.py
@@ -1,8 +1,11 @@
 import tweepy
 from secrets import *
+from randomproject import select_project
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
+
+print select_project()
