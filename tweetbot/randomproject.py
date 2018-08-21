ce0da9b57bb558c7c24a4299df39ab15a18a41e8
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-08-21 15:07:21 +0100

    Filter projects by subjects left & redirect status
    Previously I was using the API call to project.completedness, however
    this may not be a good metric for deciding which projects to promote as
    for some projects which have lots of data, having a high completedness
    score does not neccessarily equate to having little data left for
    classifications.
    
    For now, any live projects which still have more than 500 subjects for
    classification are included in the list of projects to promote.
    
    Additionally only include projects which are not built on the old
    framework (i.e. not panoptes). So if they have a redirect link, do not
    include them. This is becuase calling the data from these projects will
    be tricky and make implementation of systems for evaluation difficult.

--- tweetbot/randomproject.py
+++ tweetbot/randomproject.py
@@ -6,16 +6,28 @@ import random
 def select_project():
     project_list = {}
     for project in Project.where(launch_approved=True):
-        if project.completeness < 0.9:  # Project completeness ranges between 0 and 1
-            project_list[project.title] = [("www.zooniverse.org/projects/" + project.slug), project.description] #For twitter message? Largest is 265
-    random_project = select_random(project_list)
-    write_out(random_project)
-    return random_project
+        if not project.redirect:
+            if completedness(project) > 500:
+                if project.completeness == 1.0:
+                    print project.title
 
 
-def select_random(projects):
-    return random.choice(list(projects.items()  ))
+        #     if project.completeness < 0.9:  # Project completeness ranges between 0 and 1
+        #         project_list[project.title] = [("www.zooniverse.org/projects/" + project.slug), project.description] #For twitter message? Largest is 265
+        # random_project = select_random(project_list)
+        # write_out(random_project)
+        # return random_project
+
+def completedness(project):
+    incomplete_subjects = project.subjects_count - project.retired_subjects_count
+    return incomplete_subjects
+
+
+def select_random(project_dict):
+    return random.choice(list(projects_dict.items()  ))
 
 def write_out(string):
     with open("last-promoted-project.txt", "a") as file:
         file.write(str(string) + "\n")
+
+select_project()
