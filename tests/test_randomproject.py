e4a61c15420c3e90e33e52994b2fbc2b432d77a6
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-08-21 15:02:40 +0100

    Start a test directory for unittests

---
+++ tests/test_randomproject.py
@@ -0,0 +1,9 @@
+import unittest
+# from randomproject import *
+
+class TestRandomProjectFunction(unittest.TestCase):
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
