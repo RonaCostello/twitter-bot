73bf2f4a5f257fff7ff68f3aa01d81168e147ac8
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-08-21 10:44:54 +0100

    Convert the project list to a dictionary
    Changed the variable which holds the list of zooniverse projects from a
    list to a dictionary.

--- randomproject.py
+++ randomproject.py
@@ -4,19 +4,18 @@ from panoptes_client import Project
 import random
 
 def select_project():
-    project_list = []
+    project_list = {}
     for project in Project.where(launch_approved=True):
         if project.completeness < 0.9:  # Project completeness ranges between 0 and 1
-            project_list.append(project.title + ("__www.zooniverse.org/projects/" + project.slug))
-            # project_info.append([project.description, project.slug]) #For twitter message? Largest is 265
+            project_list[project.title] = [("www.zooniverse.org/projects/" + project.slug), project.description] #For twitter message? Largest is 265
     random_project = select_random(project_list)
     write_out(random_project)
     return random_project
 
 
-def select_random(items):
-    return random.choice(items)
+def select_random(projects):
+    return random.choice(list(projects.items()  ))
 
 def write_out(string):
     with open("last-promoted-project.txt", "a") as file:
-        file.write(string + "\n")
+        file.write(str(string) + "\n")
