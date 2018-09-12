f1cbc0a49fd9cd4888b01b36550e62e2f44a7da6
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-09-12 17:27:18 +0100

    Tweetbot now can post a short and long tweet

--- tweetbot/tweetbot.py
+++ tweetbot/tweetbot.py
@@ -1,11 +1,22 @@
 import tweepy
 from secrets import *
-from randomproject import select_project
+from randomproject import random_project
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
 auth.set_access_token(access_token, access_secret)
-
 api = tweepy.API(auth)
 
-print select_project()
+project = random_project()
+
+def simple_tweet():
+    tweet = "Check out " + project.title + " on the Zooniverse " + "https://www.zooniverse.org/projects/" + project.slug
+    api.update_status(status = tweet)
+
+
+def descriptive_tweet():
+    tweet = "Check out " + project.title + " on the Zooniverse, " + project.description + " https://www.zooniverse.org/projects/" + project.slug
+    api.update_status(status = tweet)
+
+simple_tweet()
+
+descriptive_tweet()
