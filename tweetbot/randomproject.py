c25648ea3bfc4b76aa51bd724c78d00e2fb3eaae
Rona Costello <rona.costello@plants.ox.ac.uk>
2018-09-24 16:34:29 +0100

    Correct spelling

--- tweetbot/randomproject.py
+++ tweetbot/randomproject.py
@@ -7,12 +7,11 @@ def random_project():
     project_list = []
     for project in Project.where(launch_approved=True):
         if not project.redirect:
-            if incompletedness(project) > 500:
-                if len(project.description + project.title) < 200:
-                    project_list.append(project)
+            if incompleteness(project) > 500:
+                project_list.append(project)
     return(random.choice(project_list))
 
 
-def incompletedness(project):
+def incompleteness(project):
     incomplete_subjects = project.subjects_count - project.retired_subjects_count
     return incomplete_subjects
