b40881075bf17ff62af44d09dacadf5cae7ccffa
Rona Costello <rona.costello@plants.ox.ac.uk>
2018-09-24 16:34:57 +0100

    Send random project tweet with & without image
    We want to test the effectiveness of tweeting with an image and with and
    without a project description. The bot now has functions to do all these
    options.
    
    In this commit, a short or long message is randomly chosen and it posts
    both a tweet with and without an image. Need to randomise the
    presence/absence of an image too.

--- tweetbot/send_tweet.py
+++ tweetbot/send_tweet.py
@@ -5,16 +5,37 @@ from randomproject import random_project
 import sys, pdb, pprint
 import requests
 import os
-
-def tweet_no_image(message):
-    api = twitter_api()
-    api.update_status(status = message)
+import random
 
 
-def tweet_image(url, message):
+def fetch_random_project_max_len_280(message):
+    project = random_project()
+    if length_of_tweet(project, message) > 280:
+        fetch_random_project_max_len_280(message)
+    return project
+
+def length_of_tweet(project, message):
+    twitter_url_chars = 23
+    project_name_chars = len(project.title)
+    project_description_chars = len(project.description)
+    tweet_length = twitter_url_chars + project_name_chars + project_description_chars + message
+    return tweet_length
+
+def twitter_api():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    return tweepy.API(auth)
+
+def get_avatar_url(project):
+    avatar = project.avatar
+    avatar_url = avatar['media'][0]['src']
+    return avatar_url
+
+def tweet_image(project, message):
+    image_url = get_avatar_url(project)
     api = twitter_api()
     filename = 'temp.jpg'
-    request = requests.get(url, stream=True)
+    request = requests.get(image_url, stream=True)
     if request.status_code == 200:
         with open(filename, 'wb') as image:
             for chunk in request:
@@ -24,25 +45,26 @@ def tweet_image(url, message):
     else:
         print("could not download image")
 
+def tweet_no_image(message):
+    api = twitter_api()
+    api.update_status(status = message)
 
-def twitter_api():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
-    return tweepy.API(auth)
 
 
 if __name__ == '__main__':
 
-    project = random_project()
-    print "intro:"
-    print project.introduction
-    avatar = project.avatar
-    avatar_url = avatar['media'][0]['src']
-    simple_message = "Check out " + project.title + " on the Zooniverse " + "https://www.zooniverse.org/projects/" + project.slug
-    descriptive_message = "Check out " + project.title + " on the Zooniverse - " + project.description + " https://www.zooniverse.org/projects/" + project.slug
+    message_pt1 = "Check out the project "
+    message_pt2 = " on the Zooniverse! "
+    message_len = len(message_pt1 + message_pt2)
 
-    tweet_image(avatar_url, descriptive_message)
-    tweet_no_image(descriptive_message)
+    project = fetch_random_project_max_len_280(message_len)
 
+    print(project.title)
 
-# # pp.pprint(project.__dict__)
+    simple_tweet = message_pt1 + project.title + message_pt2
+    descriptive_tweet = simple_tweet + project.description
+
+    tweet = random.choice([simple_tweet, descriptive_tweet]) + " https://www.zooniverse.org/projects/" + project.slug
+
+    tweet_image(project, tweet)
+    tweet_no_image(tweet)
